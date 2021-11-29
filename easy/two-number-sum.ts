a50280329421104cd21df3a72dfbf3dc0989b0a7
Aekant <rajaaekant123@gmail.com>
2021-11-29 23:50:28 +0400

    Added two-number-sum (easy) algorithm

---
+++ easy/two-number-sum.ts
@@ -0,0 +1,28 @@
+// given an array of integers return if we can create a given sum by adding two integers from the array
+
+
+const arr: number[] = [1, 3, 4, 11, 2, -1, 5]
+
+const twoNumberSum = (arr: number[], target: number) => {
+  const hashMap: {[index: string]: boolean} = {}
+
+  for(let num of arr) {
+    // check what is required to make the target sum
+    const complement = target - num
+
+    // if the required num exists then return true
+    if(hashMap[`${complement}`])
+      return true
+
+    hashMap[`${num}`] = true
+  }
+
+  return false
+}
+
+console.log(twoNumberSum(arr, 10))
+
+/**
+ * Time Complexity O(N)
+ * Space Complexity O(N)
+ */
